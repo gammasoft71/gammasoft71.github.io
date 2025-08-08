2a080ce72b8ab0ab7b3bd73b2853cf73813505aa
Gammasoft <gammasoft71@gmail.com>
2025-08-08 11:26:42 +0200

    Update index.js

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -7,10 +7,10 @@ const FeatureList = [
     title: 'Gammasoft is...',
     description: (
       <>
-      • The nickname of <a href="https://about.me/yves.fiumefreddo">Yves Fiumefreddo</a>.
+      • The creator of the well-known <a href="https://gammasoft71.github.io/xtd/" target="_self">xtd</a> framework.
+      <br/>• The nickname of <a href="https://about.me/yves.fiumefreddo">Yves Fiumefreddo</a>.
       <br/>• More than thirty years of passion for high technology especially in development (C++, C#, objective-c, ...).
       <br/>• Object-oriented programming is more than a mindset.
-      <br/>• The creator of the well-known <a href="https://gammasoft71.github.io/xtd/" target="_self">xtd</a> framework.
       </>
     ),
   },
